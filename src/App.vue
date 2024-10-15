7c744a1c7b625de5fe6fc6eff7e54cd15ef76f7d
Amir <amir.hossein138020@gmail.com>
2024-10-15 16:03:24 +0330

    completed project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="bg-dark d-flex align-items-center justify-content-center text-light">
+      <h3>section 1</h3>
+    </section>
+    <section class="bg-danger d-flex align-items-center justify-content-center text-light">
+      <h3>section 2</h3>
+    </section>
+    <section class="bg-primary d-flex align-items-center justify-content-center text-light">
+      <h3>section 3</h3>
+    </section>
+    <section class="bg-warning d-flex align-items-center justify-content-center text-light">
+      <h3>section 4</h3>
+    </section>
+    <section class="bg-success d-flex align-items-center justify-content-center text-light">
+      <h3>section 5</h3>
+    </section>
+  </div>
+  <transition name="slide-fade">
+    <GoToUp v-if="scroll > 400"/>
+  </transition>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import GoToUp from './components/GoToUp.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const scroll = ref(null)
+document.addEventListener('scroll',()=>{
+  scroll.value = document.documentElement.scrollTop
+  
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+section {
+  height: 100vh;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
 }
 </style>
---
+++ src/components/GoToUp.vue
@@ -0,0 +1,23 @@
+<template>
+  <button id="jsHandle" class="bg-light border-0 rounded-circle" @click="GoToUpHandler">
+    <i class="bi bi-arrow-up-circle fs-3"></i>
+  </button>
+</template>
+
+<script setup>
+// import { ref } from 'vue';
+
+function GoToUpHandler(){
+    document.documentElement.scrollTop = 0
+}
+</script>
+
+<style scoped>
+#jsHandle{
+    position: fixed;
+    bottom: 30px;
+    right: 50px;
+    width: 50px;
+    height: 50px;
+}
+</style>
